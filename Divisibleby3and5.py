edfa7a5612e5f6ee1c5e762c686a67147ec0bc8d
Tarek Aljamal <tarekaljamal@outlook.com>
2024-10-30 21:40:57 +0200

    Update Divisibleby3and5.py

--- Divisibleby3and5.py
+++ Divisibleby3and5.py
@@ -1,7 +1,7 @@
 
 # A python code the requests the user to enter a number x to check the numbers that are divisible by 3 and 5
 
-x = input("Enter the number x: ") # Asks the user to enter a number and stores it in x
+x = input("Enter the number x: ")
 
 for x in range(1,int(x)+1,1): # Checks the numbers the numbers from one to x that are divisible by 3 and 5 and prints them
     if(x%3==0 | x%5==0):
